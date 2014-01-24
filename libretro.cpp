9db93e898a985a1b3bc4b638eb9f196e63f2ae7f
twinaphex <libretro@gmail.com>
2014-01-25 00:50:31 +0100

    Increase resolutions

--- libretro.cpp
+++ libretro.cpp
@@ -65,8 +65,8 @@ static bool display_position;
 #define MAX_WIDTH 1024
 #define MAX_HEIGHT 1024
 #else
-#define MAX_WIDTH 1920
-#define MAX_HEIGHT 1600
+#define MAX_WIDTH 2048
+#define MAX_HEIGHT 2048
 #endif
 
 unsigned engine_width = BASE_WIDTH;
@@ -108,7 +108,7 @@ void retro_get_system_info(struct retro_system_info *info)
    info->library_name     = "Libretro 3DEngine";
    info->library_version  = "v1";
    info->need_fullpath    = false;
-   info->valid_extensions = "png|mtl|obj";
+   info->valid_extensions = "png|jpg|mtl|obj";
 }
 
 void retro_get_system_av_info(struct retro_system_av_info *info)
@@ -128,14 +128,14 @@ void retro_set_environment(retro_environment_t cb)
 
    struct retro_variable variables[] = {
       { "3dengine-resolution",
-#ifdef GLES
-         "Internal resolution; 320x240|360x480|480x272|512x384|512x512|640x240|640x448|640x480|720x576|800x600|960x720|1024x768" },
-#else
-      "Internal resolution; 320x240|360x480|480x272|512x384|512x512|640x240|640x448|640x480|720x576|800x600|960x720|1024x768|1024x1024|1280x720|1280x960|1600x1200|1920x1080|1920x1440|1920x1600" },
+         "Internal resolution; 320x240|360x480|480x272|512x384|512x512|640x240|640x448|640x480|720x576|800x600|960x720|1024x768"
+#ifndef GLES
+      "|1024x1024|1280x720|1280x960|1600x1200|1920x1080|1920x1440|1920x1600|2048x1152|2048x1536|2048x2048|320x240"
 #endif
+      },
                         {
          "3dengine-cube-size",
-         "Cube size; 1|2|4|8|16|32|64|128" },
+         "Cube size; 0|1|2|4|8|16|32|64|128" },
                         {
          "3dengine-cube-stride",
          "Cube stride; 2.0|3.0|4.0|5.0|6.0|7.0|8.0" },
